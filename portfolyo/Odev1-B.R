ae528facaed4797a76171e1bdbe7726be7f014a1
MuhammedEmreSendur <sendurmuhammedemre@gmail.com>
2024-03-30 13:55:32 +0300

    Odev1-B-Secenegi
    Ödev-1'in B seçeneği tamamlanmıştır. Kontrol sağlayınız.

---
+++ portfolyo/Odev1-B.R
@@ -0,0 +1,29 @@
+# mtcars veri setini yükle
+data("mtcars")
+
+# Özel Özet Fonksiyonu Yazın
+custom_summary <- function(vec) {
+  summary_list <- list(
+    Ortalama = mean(vec),
+    Medyan = median(vec),
+    Standart_Sapma = sd(vec),
+    Minimum = min(vec),
+    Maksimum = max(vec)
+  )
+  return(summary_list)
+}
+
+# Fonksiyonu Döngü Kullanarak Uygulama
+cat("Fonksiyonu Döngü Kullanarak Uygulama:\n")
+for (col_name in colnames(mtcars)) {
+  cat(col_name, ":\n")
+  cat("------------------------------\n")
+  result <- custom_summary(mtcars[[col_name]])
+  print(result)
+  cat("\n")
+}
+
+# apply ile Alternatif Bir Yaklaşım
+cat("\napply ile Alternatif Bir Yaklaşım:\n")
+apply(mtcars, 2, custom_summary)
+
